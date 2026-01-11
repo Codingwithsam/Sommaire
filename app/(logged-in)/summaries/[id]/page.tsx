f1d565dae7e6336f3857d8e503a06a0035e3984d
samuelbalogun <sambal523@gmail.com>
2026-01-11 17:04:30 -0500

    Improved layout spacing in summaries page; adjusted padding for better responsiveness across breakpoints.

--- app/(logged-in)/summaries/[id]/page.tsx
+++ app/(logged-in)/summaries/[id]/page.tsx
@@ -22,7 +22,7 @@ const SummaryPage = async (props: { params: Promise<{ id: string }> }) => {
         <div className={"relative isolate min-h-screen bg-linear-to-b from-rose-50/40 to-white"}>
             <BgGradient className={"from-rose-400 via-rose-300 to-orange-200"}/>
 
-            <div className={"container mx-auto flex flex-col gap-4"}>
+            <div className="container mx-auto px-0 sm:px-4 lg:px-8 flex flex-col gap-4">
                 <div className={"px-4 sm:px-6 lg:px-8 py-6 sm:py-12 lg:py-4"}>
                     <div className={"flex flex-col"}>
                         <SummaryHeader title={title} createdAt={created_at} readingTime={readingTime}/>
